aac733f1092d987f462489e86d10795eb4abf0b6
Joe Burgess <joe@flatironschool.com>
2014-10-09 09:09:13 -0400

    Adding empty FISRSVP

---
+++ word-play/FISRSVP.h
@@ -0,0 +1,13 @@
+//
+//  FISRSVP.h
+//  word-play
+//
+//  Created by Joe Burgess on 10/9/14.
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISRSVP : NSObject
+
+@end
---
+++ word-play/FISRSVP.m
@@ -0,0 +1,13 @@
+//
+//  FISRSVP.m
+//  word-play
+//
+//  Created by Joe Burgess on 10/9/14.
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+#import "FISRSVP.h"
+
+@implementation FISRSVP
+
+@end
